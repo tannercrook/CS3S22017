65949bddf8d77f06a5efcfcab8134677ed0d7666
pi <pi@enterpi.lcsd2.org>
2017-02-23 17:42:40 +0000

    Added view package and Main view class.

--- CS3S22017/src/view/Main.java
+++ CS3S22017/src/view/Main.java
@@ -3,13 +3,16 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package control;
+package view;
 
 /**
  *
  * @author pi
  */
 public class Main {
-   
+    
+    public static void main(String[] args) {
+        System.out.println("Hello,World!");
+    }
     
 }

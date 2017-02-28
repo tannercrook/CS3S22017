8974608f70a4f0af742bf5539444a8cc4eb51bf5
pi <pi@enterpi.lcsd2.org>
2017-02-28 17:24:20 +0000

    Added view package and Main view class.

--- CS3S22017/src/model/Hero.java
+++ CS3S22017/src/model/Hero.java
@@ -3,11 +3,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package source;
+package model;
 
 /**
  *
- * @author TannerCrook
+ * @author pi
  */
 public class Hero {
     

a98b4512f6de74c09867189f15027240e3affd19
Slime45 <Slime45@applepi.lcsd2.org>
2017-03-20 17:02:49 +0000

    added combat and inventory menu

--- CS3S22017/src/view/CombatMenu.java
+++ CS3S22017/src/view/CombatMenu.java
@@ -3,12 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package control;
+package view;
 
-/**
- *
- * @author pi
- */
-public class Inventory {
+
+public class CombatMenu extends View {
+
+    public CombatMenu() {
+        super()
+    }
     
 }

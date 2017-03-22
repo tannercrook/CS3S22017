e70f9acd020750974a8051a53ae035cce648476d
tacrook <tacrook@enterpi.lcsd2.org>
2017-03-22 16:56:09 +0000

    Updated control.InventoryControl.jabba

--- CS3S22017/src/view/View.java
+++ CS3S22017/src/view/View.java
@@ -9,21 +9,64 @@ package view;
  *
  * @author tacrook@lcsd2.org
  */
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+/**
+ *
+ * @author tacrook
+ */
+
 import java.util.Scanner;
-import CS3S22017.control;
-import static CS3S22017.control.Main.player; 
 
 public class View {
- public static void View(String[] args) { 
- System.out.println("Announcer: Today's battle is between- Jackson, 'The Sideburn Wrangler!' and, uh, wow... This is hardly even legible. Will the other competitor just announce their name to the crowd?");
- System.out.println("[ENTER YOUR NAME]");
- player.name = input.nextLine();
- 
-         
-         
     
+    protected String menu = "";
+    protected Scanner input = new Scanner(System.in);
     
-}
+    public View() {
+        this.menu = "Hello, this is a menu!\n"
+                    + "A - print 'Selected A'";
+    }
+    
+    public View(String menu) {
+        this.menu = menu;
+    }
+    
+    protected char getInput() {
+        System.out.println(this.getMenu());
+        System.out.print("Choice: ");
+        String entry = input.nextLine();
+        entry = entry.toUpperCase();
+        char choice = entry.charAt(0);
+        return choice;
+    }
+    
+    public void presentView() {
+        boolean working = true;
+        while (working) {
+            char choice = this.getInput();
+            switch (choice) {
+                case 'A':
+                    System.out.println("Selected A");
+                    working = false;
+                    break;
+                default:
+                    System.out.println("You did not select a valid option.");
+                    break;
+            }
+        }
+    }
+    
+    private String getMenu() {
+        return this.menu;
+    }
+    
 }
 
 

13ba26db4a65fd909871e3880bb032c02c830895
Slime45 <Slime45@applepi.lcsd2.org>
2017-03-24 17:04:25 +0000

    inventory

--- CS3S22017/src/model/Inventory.java
+++ CS3S22017/src/model/Inventory.java
@@ -11,4 +11,30 @@ package model;
  */
 public class Inventory {
 
+    //Size
+    int size = 5;
+    int amount = 0;
+    
+    //Items
+    Consumable[] items = new Consumable[this.size];
+    
+    public Inventory() {
+        
+    }
+    
+    public void print() {
+    
+        System.out.println();
+        System.out.println();
+        System.out.println("Slot        Item        Worth");
+        System.out.println("=============================");
+        
+        if (this.amount > 0) {
+        
+            for (int i = 0 < this.amount; i++)
+        
+        }
+    
+    }
+    
 }

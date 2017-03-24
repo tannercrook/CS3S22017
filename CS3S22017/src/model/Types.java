d754b66541de3ccee7f0d9d9a9ca6d76c4fa2b3a
pi <pi@coconutpi.lcsd2.org>
2017-03-24 17:03:38 +0000

    Merge origin/master
    Conflicts:
    	CS3S22017/src/view/MainMenu.java

--- CS3S22017/src/model/Types.java
+++ CS3S22017/src/model/Types.java
@@ -12,12 +12,27 @@ import model.Hero;
 public class Types {
     public void knight(){
         player.setMaxHealth(player.maxHealth + 4);
+        player.setCurrentHealth();
+        player.setMaxStamina();
+        player.setCurrentStamina();
+        player.setStrength();
+        player.setSpeed(player.speed - 2);
+        player.setDefense();
+        player.setCurrentExp();
+        player.setMaxExp();
+        player.setSealPoints();
         
     }
     public void assassin(){
         player.setMaxHealth(player.maxHealth + 2);
+        player.setCurrentHealth();
+        player.setMaxStamina();
+        player.setCurrentStamina();
+        player.setStrength();
+        player.setSpeed(player.speed + 5);
     }
     public void barbarian(){
         player.setMaxHealth(player.maxHealth);
+        player.setSpeed(player.speed);
     }
 }

60dcec59b3df07871ff85612793443640184e475
pi <pi@peachpi.lcsd2.org>
2017-04-13 15:55:51 +0000

    Almost there

--- CS3S22017/src/control/Main.java
+++ CS3S22017/src/control/Main.java
@@ -24,19 +24,21 @@ public static Entity Jackson;
 public static void main(String[] args) {
     player = new Hero();
     
-    Intro tutorial = new Intro();
-    
-    tutorial.gameSetup();
-    
-    //Sword bladeOfWoe = new Sword("Blade of Woe"(Name),100(Value),"Test Sword"(Desc),50(Damage),10(level));
+        //Sword bladeOfWoe = new Sword("Blade of Woe"(Name),100(Value),"Test Sword"(Desc),50(Damage),10(level));
 Sword trainingSword = new Sword("Training Sword",1,"Sword of the Starts",2,1);
 Axe barbAxe = new Axe("Barbariac Axe",10,"Axe from a Barbarian",4,2);
 
 player.setMainWeapon(trainingSword);
 
-
+ 
 Lance llamaSlayer = new Lance("Llama Slayer",1000,"The Weapon made of steel and Llama fur",10,5);
-//Axe larkScythe = new Axe("Lark Scythe")    
+//Axe larkScythe = new Axe("Lark Scythe") 
+    
+    Intro tutorial = new Intro();
+    
+    tutorial.gameSetup();
+    
+   
     
 }
 

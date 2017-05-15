ac8707f28a13283028c98021a242020fc62205f0
pi <pi@peachpi.lcsd2.org>
2017-05-15 17:03:54 +0000

    Added arenaAction(); to main. Should be good now ^*^

--- CS3S22017/src/control/TutorialFight.java
+++ CS3S22017/src/control/TutorialFight.java
@@ -7,7 +7,8 @@ import static jdk.nashorn.tools.ShellFunctions.input;
 import  model.Hero;
 import model.Weapon;
 import model. *;
-public class Combat {
+import view.*;
+
 
     
  /* Arena has 3 options: 1- Find a Fight, 2- Redo Tutorial, 3- Challenge next boss.(auto enters into tutorial) 
@@ -21,12 +22,25 @@ public class Combat {
     8. Tanner, The Bearded King 
     9. Create "Beardslayer"  
  */   
-
-    private static class tutorialFight {
-        
+    
+    public class TutorialFight {
+       Scanner input = new Scanner(System.in);
+       Scanner query = new Scanner(System.in);
+        public void arenaStart(){
+           System.out.println("Welcome to the arena! Are you fixin' to fight?");
+           System.out.println("Y/N");
+           String answer = query.nextLine();
+           if(answer == "N" ){
+               MainMenu mainMenu = new MainMenu();
+               mainMenu.inTown();
+               
+           }else if(answer == "Y"){
+               arenaAction();
+           }
+       }
         
         Entity jackson = new Entity();
-        Scanner input = new Scanner(System.in);  
+          
         protected char getInput() {
    
         System.out.print("Choice: ");
@@ -35,6 +49,17 @@ public class Combat {
         char choice = entry.charAt(0);
         return choice;
     }  
+        
+        private void prepareTutorial() {
+            jackson.setSpeed(1);
+            jackson.setStrength(1);
+            jackson.setMaxHealth(16);
+            jackson.setLevel(1);
+            jackson.setMaxStanina(10);
+            jackson.setDefense(1);
+            jackson.setSealPoints(10);
+        }
+        
         private void block(){
             System.out.println( player.getName() + "decided to block!");    
         }
@@ -52,14 +77,16 @@ public class Combat {
           int resultDamage = player.getCurrentHealth() - this.totalDamage(jackson);
           return resultDamage;
         }
+        
         private void dialogue1(){
         System.out.println("Here ya go, " + player.getName() + "the tutorial.");
         System.out.println("Jackson, The Sideburn Wrangler, has entered the arena!");
         System.out.println("Welcome to the tutoral, " + player.getName() + "! Wilfred, The Godslayer and I will see to it that you get educated!");   
         }
         
-        private void arenaAction() {  
-            
+        public void arenaAction() {  
+            dialogue1();
+            prepareTutorial();
             System.out.println("So, you're fighting, are ya? Well, what do ya want to do? A = Attack, B = Block, I = Item, C = Concede, H = Help " );
             System.out.println("Choice: ");
 
@@ -75,28 +102,43 @@ public class Combat {
                 case 'A': 
                     player.getStrength();
                     player.attack(jackson);  
+                    
+                    if((player.getSpeed() - jackson.getSpeed()) > 6){
+                        player.attack(jackson);
+                    }
                     jackson.attack(player);
-                           
+                    arenaAction();
                     break;
                 case 'B': 
                     block();
                     player.getMaxHealth();
                     player.getBlockDefense();
                     jackson.blockedAttack(player);
+                    System.out.println("You took " + this.totalDamage(jackson) + " damage!");
+                    arenaAction();
                     break;
                 case 'C':
-                    
+                    System.out.println("Aw, giving up already? Well, I don't blame you.");
+                    System.out.println(player.getName() + " conceded!");
+                    System.out.println("Jackson: Hah! Weak! Come back when you grow a pair!");
+                    MainMenu mainMenu = new MainMenu();
+                    mainMenu.inTown();
                     break;
                 case 'H':
                     System.out.println("Hey! my name's Clippy, and I'm here to help! What do you need explained?/n");
                     System.out.println("1 - Combat, 2 - Blocking, 3 - Conceding/n");
                     System.out.println("Choice:/n");
                     int helpChoice = input.nextInt();
-                    
+                    break;
+                case 'I':
+                    InventoryMenu itemsView = new InventoryMenu();
+                    itemsView.presentView();
+                    arenaAction();
+                    break;
                     case 1: 
                         System.out.println("Alright, I'll tell you what I know about combat./n"
                                 + "Combat is the main point of this game. In combat, you have three options./n"
-                                + "You can attack, block, or defend. ");
+                                + "You can attack, block, or defend. If your speed is at least 6 higher than the opposition, you attack twice.");
                         break;
                     case 2:
                         System.out.println("When you block, your defense is doubled, however you still take damage."  );
@@ -104,13 +146,16 @@ public class Combat {
                     case 3:    
                         System.out.println("This seems fairly self- explanatory, but since you pushed '3', I'll tell you anyway./n "
                                 + "When you concede, you automatically lose the fight and any resources you expended during that fight.");
+                        break;
+                        
 
             }
         }
 
-       
+      
     }
-}
+    
+
     
 
 
